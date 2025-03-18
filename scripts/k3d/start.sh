38da70249a951da6a7ee39713a5138e364ae3ab6
Mojtaba Imani <sm.imani@gmail.com>
2025-03-18 07:41:05 +0330

    Add docker registry proxy setup and k3d cluster configuration

---
+++ scripts/k3d/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run the docker registry proxy script first
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+"$SCRIPT_DIR"/docker-registry-proxy.sh
+
+export PROXY_HOST=registry-proxy
+export PROXY_PORT=3128
+export NOPROXY_LIST="localhost,127.0.0.1,0.0.0.0,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,.local,.svc"
+
+# to keep the pulled images when you delete and recreate the cluster
+mkdir -p ~/tmp/k3d_docker_images
+chmod 755 ~/tmp/k3d_docker_images
+
+if k3d cluster get shared >/dev/null 2>&1; then
+  echo "k3d cluster 'shared' already exists"
+else
+  k3d cluster create --config "$SCRIPT_DIR"/shared.yaml
+fi
--- scripts/more/keycloak-export-realm+users.sh
+++ scripts/more/keycloak-export-realm+users.sh
@@ -2,7 +2,7 @@
 # This script exports the Keycloak realm configuration
 
 kcadm.sh config credentials --server  https://keycloak.onprem.kuberise.dev --realm master --user admin --password admin
-kc.sh export --realm platform --users realm_file --dir /Users/mojtaba/repo/github/kuberise/kuberise
+kc.sh export --realm platform --users realm_file --dir ~/repo/github/kuberise/kuberise
 
 
 quay.io/keycloak/keycloak-operator:19.0.3-legacy
